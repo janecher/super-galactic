39d5e339719a6a094cd2356a58ed86c16b44bdd9
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-26 09:11:51 -0700

    add test for venusAge(), failed it before create venusAge function, and then pass after creation

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,6 +1,7 @@
 import { Person } from './../src/person.js';
 
 describe('create Person object', () => {
+
   test('should return object with age property', () => {
     const person = new Person(34);
     expect(person.age).toBe(34);
@@ -8,8 +9,14 @@ describe('create Person object', () => {
 });
 
 describe('calculate person age on different planets', () => {
+
   test('should calculate Mercury age', () => {
     const person = new Person(34);
     expect(person.mercuryAge()).toEqual(141);
+  });
+
+  test('should calculate Venus age', () => {
+    const person = new Person(34);
+    expect(person.venusAge()).toEqual(54);
   });
 });
