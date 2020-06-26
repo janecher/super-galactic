75bf5e10313da0dc0c929bd9c4384ad0503657ff
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-26 08:59:39 -0700

    add test for checking mercuryAge function, failed it - function is not created yet

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -6,3 +6,10 @@ describe('create Person object', () => {
     expect(person.age).toBe(34);
   });
 });
+
+describe('calculate person age on different planets', () => {
+  test('should calculate Mercury age', () => {
+    const person = new Person(34);
+    expect(person.mercuryAge()).toEqual(141);
+  });
+});
