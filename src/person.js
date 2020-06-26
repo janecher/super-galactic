8d21762735adf6777c5b8b10f74e03ef68a617ab
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-26 09:02:02 -0700

    create mercuryAge() function

--- src/person.js
+++ src/person.js
@@ -1,5 +1,10 @@
-class Person {
+export class Person {
   constructor(age) {
     this.age = age;
+  }
+
+  mercuryAge() {
+    const mercury = 0.24;
+    return Math.floor(this.age / mercury);
   }
 }
